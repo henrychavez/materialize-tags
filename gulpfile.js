b2b0c17f8f45b357350a3c698ea46551f65075b4
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2017-09-25 15:38:06 +0200

    Gulpfile and Package updated to preprend a header to dist files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp         = require('gulp'),
-    minifyCss    = require('gulp-clean-css'),
+    cleanCSS     = require('gulp-clean-css')
     concat       = require('gulp-concat'),
+    header       = require('gulp-header');
     rename       = require("gulp-rename"),
     rimraf       = require('gulp-rimraf'),
     sass         = require("gulp-sass"), // https://www.npmjs.org/package/gulp-sass
@@ -9,10 +10,23 @@ var gulp         = require('gulp'),
     util         = require("gulp-util"), // https://github.com/gulpjs/gulp-util
     zip          = require('gulp-zip');
 
+// using data from package.json
+var pkg = require('./package.json');
+var banner = ['/**',
+  ' * <%= pkg.name %> - <%= pkg.description %>',
+  ' * @version v<%= pkg.version %>',
+  ' * @link <%= pkg.homepage %>',
+  ' * @license <%= pkg.license %>',
+  ' * @author <%= pkg.author %>',
+  ' * @maintainer <%= pkg.maintainers[0].name %> <<%= pkg.maintainers[0].email %>>',
+  ' */',
+  ''].join('\n');
+
 gulp.task('js', function ()
 {
     // place code for your default task here
     return gulp.src('src/*.js')
+        .pipe(header(banner, { pkg : pkg } ))
         .pipe(gulp.dest('dist/js'))
         .pipe(sourcemaps.init())
         .pipe(uglify())
@@ -20,6 +34,7 @@ gulp.task('js', function ()
             extname : '.min.js'
         }))
         .pipe(sourcemaps.write('./'))
+        .pipe(header(banner, { pkg : pkg } ))
         .pipe(gulp.dest('dist/js'));
 });
 
@@ -27,13 +42,15 @@ gulp.task('css', function ()
 {
     // place code for your default task here
     return gulp.src('src/*.css')
+        .pipe(header(banner, { pkg : pkg } ))
         .pipe(gulp.dest('dist/css'))
         .pipe(sourcemaps.init())
-        .pipe(minifyCss({compatibility : 'ie8'}))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(rename({
             extname : '.min.css'
         }))
         .pipe(sourcemaps.write('./'))
+        .pipe(header(banner, { pkg : pkg } ))
         .pipe(gulp.dest('dist/css'));
 });
 
